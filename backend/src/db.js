45e0791ffb56b836ef75cff8b988caf00d1a3ea2
smile04455660 <smile04455660@gmail.com>
2022-12-22 00:07:24 +0800

    add db schema, modify connection config

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 import dotenv from "dotenv-defaults"
 
 export default {
     connect: () => {
         dotenv.config()
-        mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((res) => console.log("mongo db connection created"))
-    }
+        if (!process.env.MONGO_URL) {
+            throw "MONGO_URL not found!!"
+        }
+        mongoose
+            .connect(process.env.MONGO_URL, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                dbName: "web_final",
+            })
+            .then((res) => console.log("mongo db connection created"))
+    },
 }
