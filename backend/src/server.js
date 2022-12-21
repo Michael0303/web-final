78aac26297e726c36494d7d0a6ce0a398c7dc742
smile04455660 <smile04455660@gmail.com>
2022-12-22 00:09:11 +0800

    comment unused code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import cors from 'cors'
-import db from './db'
-import routes from './routes'
-import path from 'path'
+import express from "express"
+import cors from "cors"
+import db from "./db"
+import routes from "./routes"
+import path from "path"
 
 db.connect()
 const app = express()
@@ -10,15 +10,31 @@ if (process.env.NODE_ENV !== "production") {
     app.use(cors())
 }
 app.use(express.json())
-app.use('/', routes)
+app.use("/", routes)
+// app.use("/", (req, res, next) => {
+//     var options = {
+//         root: path.join(__dirname),
+//     }
 
-if (process.env.NODE_ENV === "production") {
-    const _dirname = path.resolve()
-    app.use(express.static(path.join(_dirname, "../frontend", "build")))
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(_dirname, "../frontend", "build", "index.html"))
-    })
-}
+//     var fileName = "test.txt"
+//     res.sendFile(fileName, options, function (err) {
+//         if (err) {
+//             next(err)
+//         } else {
+//             console.log("Sent:", fileName)
+//             next()
+//         }
+//     })
+// })
+// app.use('/', routes)
+
+// if (process.env.NODE_ENV === "production") {
+//     const _dirname = path.resolve()
+//     app.use(express.static(path.join(_dirname, "../frontend", "build")))
+//     app.get("/*", (req, res) => {
+//         res.sendFile(path.join(_dirname, "../frontend", "build", "index.html"))
+//     })
+// }
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
